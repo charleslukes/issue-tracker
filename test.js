dba68499fe80d9158f9582b21f296280c0d02f8f
charleslukes <charleslukes28@gmail.com>
2023-04-10 17:57:44 +0100

    add test file

---
+++ test.js
@@ -0,0 +1,14 @@
+//TODO: GET MAPPED VALUES and return value
+//Description: To get the values we need to ensure values are properly moved 
+function mapper() {
+    const m = {a: 1, b: 2};
+    return m; 
+}
+
+//TODO: GET THE RESULT OUT OF DB
+//Description: To return the values each time a call is made
+
+function resultDB() {
+    console.log("values out")
+}
+
