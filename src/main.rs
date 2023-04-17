1f0e7ebd17fb03c0fc6e0631c9f7e7dfd6dfea05
charleslukes <charleslukes28@gmail.com>
2023-04-17 18:12:49 +0100

    update main add questions and services

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
+mod custom_error;
 mod file_data;
 mod issues_repo;
-mod custom_error;
+mod questions;
+mod service;
 mod shared;
 
-use std::{env};
+use crate::{file_data::FileData, issues_repo::Issues, questions::Question};
 use custom_error::Error;
-use crate::{file_data::FileData, issues_repo::Issues};
-
+use service::Service;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let file_data = FileData::new(&args[1]);
     let todos = FileData::create_todo(&file_data.file_content);
-    let user_issues = Issues::get_issues().await?;
-    println!("{:?}", user_issues);
-    Issues::create_all_issues(todos, &user_issues).await?;
+
+    let all_questions = vec![
+        Question {
+            name: String::from("Whats your github username?"),
+            value: String::from(""),
+            kind: questions::QuestionType::USERNAME,
+        },
+        Question {
+            name: String::from("Whats your repo name?"),
+            value: String::from(""),
+            kind: questions::QuestionType::REPONAME,
+        },
+    ];
+
+    let answers = Question::ask_reply(all_questions);
+
+    // init service
+    // no need to loop through the answers we can get them by index
+    let service = Service {
+        username: &answers[0].answer,
+        repo_name: &answers[1].answer,
+    };
+
+    let user_issues = Issues::get_issues(&service).await?;
+    Issues::create_all_issues(todos, &user_issues, &service).await?;
     Ok(())
 }
