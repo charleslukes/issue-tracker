dc79fdf86a5b6130e207926c3acaad6a526dc9fb
charleslukes <charleslukes28@gmail.com>
2023-04-18 11:35:20 +0100

    update todos

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 //TODO: GET MAPPED VALUES and return value
-//Description: To get the values we need to ensure values are properly moved 
+//Description: To get the values we need to ensure values are properly moved
 function mapper() {
-    const m = {a: 1, b: 2};
-    return m; 
+  const m = { a: 1, b: 2 };
+  return m;
 }
 
 //TODO: GET THE RESULT OUT OF DB
 //Description: To return the values each time a call is made
 
 function resultDB() {
-    console.log("values out")
+  console.log("values out");
 }
 
+// TODO: FETCH merchants supported token from the backend
+//Description: Since all merchants has ability to choose tokens only send chosen tokens by the merchant.
+
+// TODO: UPDATE USERS DETAILS AFTER PAYMENT
+// DESCRIPTION: After payment map users card as valid 
